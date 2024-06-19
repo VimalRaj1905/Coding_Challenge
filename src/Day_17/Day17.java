d2c29a6d5e0b2e15db6636bd17c3b2ae31d66123
VimalRaj1905 <vimalraj.1950@gmail.com>
2024-06-19 22:25:20 +0530

    Day 17 completed

---
+++ src/Day_17/Day17.java
@@ -0,0 +1,52 @@
+package Day_17;
+
+public class Day17 {
+    public static void main(String[] args) {
+
+        int[] bloomDay = {1, 10, 3, 10, 2};
+        int m = 3;
+        int k = 1;
+        System.out.println(new Day17().minDays(bloomDay, m, k));
+    }
+
+    public int minDays(int[] bloomDay, int m, int k) {
+        if ((long) m * k > bloomDay.length) {
+            return -1;
+        }
+        int low = 1, high = Integer.MAX_VALUE;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+
+            if (isPossibleBouquets(bloomDay, m, k, mid)) {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+
+    private boolean isPossibleBouquets(int[] bloomDay, int m, int k, int day) {
+        int total = 0;
+
+        for (int i = 0; i < bloomDay.length; i++) {
+            int count = 0;
+            while (i < bloomDay.length && count < k && bloomDay[i] <= day) {
+                count++;
+                i++;
+            }
+
+            if (count == k) {
+                total++;
+                i--;
+            }
+
+            if (total >= m) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
+
