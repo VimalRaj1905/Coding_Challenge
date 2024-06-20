a916abd4ebb83d73726e10450bbd84c00658f471
VimalRaj1905 <vimalraj.1950@gmail.com>
2024-06-20 18:09:47 +0530

    Day 18 completed

---
+++ src/Day_18/Day18.java
@@ -0,0 +1,53 @@
+package Day_18;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+
+public class Day18 {
+
+
+    public static void main(String[] args) {
+        int [] arr = {1,2,3,4,7};
+        int m = 3;
+        System.out.println(new Day18().maxDistance(arr,m));
+
+    }
+
+        public int maxDistance(int[] position, int m) {
+
+            Arrays.sort(position);
+
+            int start = 1;
+            int end = (position[position.length - 1] - position[0]) / (m - 1);
+
+            while (start <= end) {
+                int mid = (end + start) / 2;
+                int req = dist(position, mid);
+
+                if (req >= m) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            }
+
+            return end;
+        }
+
+        public int dist(int[] nums, int mid) {
+            int count = 1;
+            int last = nums[0];
+
+            for (int i = 1; i < nums.length; i++) {
+                if (nums[i] - last >= mid) {
+                    count++;
+                    last = nums[i];
+                }
+            }
+
+            return count;
+        }
+    }
+
