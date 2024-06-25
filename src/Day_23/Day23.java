e6f4b98d8efa2bba10d24f38ea80bafab8572302
VimalRaj1905 <vimalraj.1950@gmail.com>
2024-06-25 22:42:46 +0530

    Day 21 completed

---
+++ src/Day_23/Day23.java
@@ -0,0 +1,26 @@
+package Day_23;
+
+public class Day23 {
+    private int sum = 0;
+    public static void main(String[] args) {
+        String str = "sadbusted";
+        String a = "sad";
+        System.out.println(new Day23().strStr(str,a));
+
+
+    }
+    public int strStr(String haystack, String needle) {
+        if (haystack.length() < needle.length()) {
+            return -1;
+        }
+
+        for (int i = 0; i <= haystack.length() - needle.length(); i++) {
+            if (haystack.substring(i, i + needle.length()).equals(needle)) {
+                return i;
+            }
+        }
+
+        return -1;
+
+    }
+}
